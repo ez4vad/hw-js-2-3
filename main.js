4abdbb5cec6d41381e3393e4fbb4ef8c27ce8ab2
[vadym] <[itsvadikk@gmail.com]>
2024-07-28 13:04:16 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,26 @@
+// min 1
+// const num1 = 0.1;
+// const num2 = 0.2;
+// const result = num1 + num2;
+// console.log(result.toFixed(1) );
+
+
+//min 3
+// const value = prompt("give disk size");
+// const mb = value * 1000;
+// const file = 820;
+// const result = mb/file;
+// if (file <= mb) {
+//     alert(Math.floor(result))
+// } else {
+//     alert("not enough space")
+// }
+
+
+// max 1
+// const money = prompt("give summe");
+// const prise = prompt("give prise");
+// const result = money/prise;
+// const rest = result-Math.floor(result);
+// alert("can buy" + " " + Math.floor(result) + " " + "scoko");
+// alert("rest" + " " + rest.toFixed(1));

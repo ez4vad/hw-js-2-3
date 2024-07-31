365e4037c069955ad45f60f866f3afa527d825c7
[vadym] <[itsvadikk@gmail.com]>
2024-07-31 20:40:36 +0200

    commit hw js 2 ready

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//hw js 2
+
 // min 1
 // const num1 = 0.1;
 // const num2 = 0.2;
@@ -17,10 +19,102 @@
 // }
 
 
-// max 1
+// norma 1
 // const money = prompt("give summe");
 // const prise = prompt("give prise");
 // const result = money/prise;
 // const rest = result-Math.floor(result);
 // alert("can buy" + " " + Math.floor(result) + " " + "scoko");
 // alert("rest" + " " + rest.toFixed(1));
+
+
+// norma 2  (2)
+// const num = prompt("num");
+// console.log(num[2], num[1], num[0]);
+
+
+// max1
+// const dep = prompt("give a summe");
+// const procent = 10;
+// const result = (dep * procent) / 100;
+// alert("procent" + " " + result);
+
+
+//hw js 3 
+
+//min 1 rev
+// const age = prompt("give age");
+
+// if (age <= 11) {
+//     alert("kid")
+// }
+// else if (age <= 17) {
+//     alert("teenager")
+// }
+// else if (age <= 59) {
+//     alert("adult")
+// } 
+// else if (age >= 59) {
+//     alert("pensioner")
+// } else (age >= 120)
+//     alert("error")
+
+
+// min 2
+// const num = prompt("give num 0-9")
+// if (num == 0) {
+//     alert(")")
+// }
+// else if (num == 1) {
+//     alert("1")
+// }
+// else if (num == 2) {
+//     alert("@")
+// }
+// else if (num == 3) {
+//     alert("#")
+// }
+// else if (num == 4) {
+//     alert("$")
+// }
+// else if (num == 5) {
+//     alert("%")
+// }
+// else if (num == 6) {
+//     alert("^")
+// }
+// else if (num == 7) {
+//     alert("&")
+// }
+// else if (num == 8) {
+//     alert("*")
+// }
+// else if (num == 9) {
+//     alert("(")
+// }
+
+//norma 2
+// const summe = prompt("give a summe");
+// const discount = summe - (summe * 3) / 100;
+// const discount2 = summe - (summe * 5) / 100;
+// const discount3 = summe - (summe * 7) / 100;
+// if (summe <= 300) {
+//     alert(discount)
+// }
+// else if (summe <=500) {
+//     alert(discount2)
+// } else {
+//     alert(discount3)
+// }
+
+
+//min 3
+// var num = Number(prompt("give a num"));
+// var num2 = Number(prompt("give a num 2"));
+// var summe = 0;
+
+// while (num <= num2) {
+//     summe+=num;
+//     num++
+// }
+// alert(summe);  
